0577fe0dc20e5f59f3d0df0465e699c4b85eae13
liuchunhua <liuchunhua-2000@163.com>
2013-07-10 21:24:48 +0800

    使用面向对象重构

--- OSS_C/HashTable.c
+++ OSS_C/HashTable.c
@@ -1,8 +1,8 @@
 /*
- * HashTable.c
+ *HashTable.c
  *
- *  Created on: 2012-9-24
- *      Author: lch
+ * Created on: 2012-9-24
+ *     Author: lch
  */
 
 #include "HashTable.h"
@@ -12,16 +12,16 @@
 #include <stdlib.h>
 #include <assert.h>
 
-static void free_pair(struct pair* p){
+static void free_pair(struct pair *p){
 	if(p){
 		free(p->key);
 		free(p);
 	}
 }
 
-struct HashTable* hash_table_init_size(unsigned int size) {
-        if(size < HASH_TABLE_LEN) size = HASH_TABLE_LEN;
-	struct HashTable* hash_table = (struct HashTable*) malloc(sizeof(struct HashTable));
+struct HashTable *hash_table_init_size(unsigned int size) {
+	if(size < HASH_TABLE_LEN) size = HASH_TABLE_LEN;
+	struct HashTable *hash_table = (struct HashTable*) malloc(sizeof(struct HashTable));
 	memset(hash_table,0x0,sizeof(struct HashTable));
 	hash_table->hash = strlen;
 	int i = 0;
@@ -31,20 +31,20 @@ struct HashTable* hash_table_init_size(unsigned int size) {
 	}
 	return hash_table;
 }
-struct HashTable* hash_table_init(){
-  return hash_table_init_size(0);
+struct HashTable *hash_table_init(){
+	return hash_table_init_size(0);
 }
-int hash_table_put(struct HashTable* table,const char* key, void* value) {
+int hash_table_put(struct HashTable *table,const char *key, void *value) {
 	assert(table->hash!=NULL);
-	int v = (table->hash)(key);
+	int v = (table->hash)(key)%HASH_TABLE_LEN;
 	if (NULL != table->elements[v]) {
-		if (table->keys[v] == SINGLE) {
-			struct pair* key_value = (struct pair*) table->elements[v];
+		if (table->keys[v] == SINGLE) {//将单点改为链表
+			struct pair *key_value = (struct pair*) table->elements[v];
 			table->elements[v] = listInit();
 			listAdd((List) table->elements[v], key_value);
 			table->keys[v] = LIST;
 		}
-		struct pair* new_pair = (struct pair*) malloc(sizeof(struct pair));
+		struct pair *new_pair = (struct pair*) malloc(sizeof(struct pair));
 		new_pair->key = strdup(key);
 		new_pair->value = value;
 		listAdd((List) table->elements[v], new_pair);
@@ -56,17 +56,17 @@ int hash_table_put(struct HashTable* table,const char* key, void* value) {
 	((struct pair*) (table->elements[v]))->value = value;
 	return 0;
 }
-void* hash_table_get(struct HashTable* table,const char* key) {
-	int v = table->hash(key);
+struct pair *hash_table_get(struct HashTable *table,const char *key) {
+	int v = (table->hash)(key)%HASH_TABLE_LEN;
 	if (table->keys[v] == NONE)
 		return NULL ;
 	if (table->keys[v] == SINGLE) {
-		return ((struct pair*) table->elements[v])->value;
+		return ((struct pair*)table->elements[v])->value;
 	} else {
 		List list = (List) (table->elements[v]);
 		List node = list->next;
 		while (list != node->next) {
-			struct pair* map = (struct pair*) node->ptr;
+			struct pair *map = (struct pair*) node->ptr;
 			if (strcmp(key, map->key) == 0)
 				return ((struct pair*) (node->ptr))->value;
 			node = node->next;
@@ -76,60 +76,95 @@ void* hash_table_get(struct HashTable* table,const char* key) {
 
 }
 
-List hash_table_get_key_list(struct HashTable* table) {
+List hash_table_get_list(struct HashTable *table,const char *key)
+{
+	List ls = listInit();
+	int v = (table->hash)(key)%HASH_TABLE_LEN;
+	if (table->keys[v] == NONE)
+		return NULL ;
+	if (table->keys[v] == SINGLE) {
+		struct pair *pair = table->elements[v];
+		listAdd(ls,pair->value);
+		return ls;
+	} else {
+		List list = (List) (table->elements[v]);
+		List node = list->next;
+		while (list != node->next) {
+			struct pair *map = (struct pair*) node->ptr;
+			if (strcmp(key, map->key) == 0)
+				listAdd(ls, ((struct pair*) (node->ptr))->value);
+			node = node->next;
+		}
+		return ls;
+	}
+
+}
+
+List hash_table_get_all(struct HashTable *table) {
 	int i = 0;
 	List list = listInit();
 	for (; i < HASH_TABLE_LEN; i++) {
 		switch (table->keys[i]) {
-		case NONE:
-			continue;
-			break;
-		case SINGLE:
-		{
-			struct pair* p = (struct pair*) (table->elements[i]);
-			listAdd(list, (void*) p);
-			break;
-		}
-		case LIST:
-		{
-			List ls = (List) (table->elements[i]);
-			List node;
-			for_each(node,ls)
-			{
-				listAdd(list, node->ptr);
-			}
-			break;
-		}
-		default:
-			break;
+			case NONE:
+				continue;
+				break;
+			case SINGLE:
+				{
+					struct pair *p = (struct pair*) (table->elements[i]);
+					listAdd(list, (void*) p);
+					break;
+				}
+			case LIST:
+				{
+					List ls = (List) (table->elements[i]);
+					List node;
+					for_each(node,ls)
+					{
+						listAdd(list, node->ptr);
+					}
+					break;
+				}
+			default:
+				break;
 		}
 	}
 	return list;
 }
 
-void hash_table_free(struct HashTable* table){
+void hash_table_free(struct HashTable *table){
+    hash_table_free_fun(table, NULL);
+}
+void hash_table_free_fun(HashTable *table, void (*free_fun)(void *)){
 	int i = 0;
 	for (; i < HASH_TABLE_LEN; i++) {
 		switch (table->keys[i]) {
-		case NONE:
-			continue;
-			break;
-		case SINGLE:
-			free_pair((struct pair*)table->elements[i]);
-			break;
-		case LIST:
-		{
-			List ls = (List) (table->elements[i]);
-			List node;
-			for_each(node,ls)
-			{
-				free_pair((struct pair*)node->ptr);
-			}
-			listFree(ls);
-			break;
-		}
-		default:
-			break;
+			case NONE:
+				continue;
+				break;
+			case SINGLE:
+    			{
+    				struct pair *pair = table->elements[i]; 
+    				if(free_fun != NULL)
+    					(*free_fun)(pair->value);
+    				free_pair(pair);
+    				break;
+    			}
+			case LIST:
+				{
+					List ls = (List) (table->elements[i]);
+					List node;
+					for_each(node,ls)
+					{
+        				struct pair *pair = table->elements[i]; 
+        				if(free_fun != NULL)
+        					(*free_fun)(pair->value);
+        				free_pair(pair);
+					}
+					listFree(ls);
+					break;
+				}
+			default:
+				break;
 		}
 	}
 	free(table);
--- OSS_C/HashTable.h
+++ OSS_C/HashTable.h
@@ -18,35 +18,60 @@ typedef enum{NONE,SINGLE,LIST} HASHKEY;
 
 struct HashTable{
 	size_t (*hash)(const char*);
-	void* elements[HASH_TABLE_LEN];
+	void *elements[HASH_TABLE_LEN];
 	HASHKEY keys[HASH_TABLE_LEN];
 };
 
+typedef struct HashTable HashTable;
+
 struct pair{
-	void* key;
-	void* value;
+	void *key;
+	void *value;
 };
 
+typedef struct {
+	 HashTable *(*init)();
+	 HashTable *(*init_size)(unsigned int);
+	 void (*destroy)(HashTable *);
+	 void (*destroy_fun)(HashTable *, void (*free)(void *));
+	 struct pair *(*get)(HashTable *, const char *key);
+	 int (*put)(HashTable *, const char *key, void *value);
+	 List (*get_list)(HashTable *, const char *key);
+	 List (*get_all)(HashTable *);
+}HashTableOpration;
 /*
-* 初始化hash table
+ *初始化hash table
 */
-struct HashTable*  hash_table_init();
+struct HashTable *hash_table_init();
 
-struct HashTable*  hash_table_init_size(unsigned int size );
+struct HashTable *hash_table_init_size(unsigned int size );
 
 /*
-* 添加key-value
+ *添加key-value
 */
-int hash_table_put(struct HashTable* table, const char* key, void* value);
+int hash_table_put(struct HashTable *table, const char *key, void *value);
 
 /*
-* 获得key-value
+ *获得key-value
 */
+struct pair *hash_table_get(struct HashTable *table,const char *key);
+	
+//获得key的list值
+List hash_table_get_list(struct HashTable *table,const char *key);
 
-void* hash_table_get(struct HashTable* table,const char* key);
+//获得所有key-value
+List hash_table_get_all(struct HashTable *table);
 
-List hash_table_get_key_list(struct HashTable* table);
+void hash_table_free(struct HashTable *table);
+void hash_table_free_fun(HashTable *table, void (*free)(void *));
 
-void hash_table_free(struct HashTable* table);
-
+HashTableOpration HashTableClass = {
+	.init = hash_table_init,
+	.init_size = hash_table_init_size,
+	.destroy = hash_table_free,
+	.destroy_fun = hash_table_free_fun,
+	.get = hash_table_get,
+	.get_list = hash_table_get_list,
+	.get_all = hash_table_get_all
+};
 #endif /* HASHTABLE_H_ */
